e6db9dff349d3438a4cc87a359b75a9d64df326d
bayusett <bayusetyaji13@gmail.com>
2022-12-14 16:04:31 +0700

    first commit

---
+++ lib/ui/pages/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:my_app_first/shared/theme.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: const CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        elevation: 0,
+        child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: whiteColor,
+            elevation: 0,
+            selectedItemColor: blueColor,
+            unselectedItemColor: blackColor,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedLabelStyle:
+                blueTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+            unselectedLabelStyle:
+                blackTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+            items: [
+              BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/ic_more.png',
+                    width: 20,
+                    color: blueColor,
+                  ),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/ic_send.png',
+                    width: 20,
+                  ),
+                  label: 'Transfer'),
+              BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/ic_wallet.png',
+                    width: 20,
+                  ),
+                  label: 'Rekeningku'),
+              BottomNavigationBarItem(
+                  icon: Image.asset(
+                    'assets/ic_overview.png',
+                    width: 20,
+                  ),
+                  label: 'More'),
+            ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: purpleColor,
+        child: Image.asset(
+          'assets/qrcode.png',
+          width: 24,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
---
+++ lib/ui/widgets/forms.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:my_app_first/shared/theme.dart';
+
+class CustomFormField extends StatelessWidget {
+  final String title;
+  final bool obscureText;
+  final TextEditingController? controller;
+  // final bool isShowTitle;
+  // final Function(String)? onFieldSubmitted;
+
+  const CustomFormField({
+    Key? key,
+    required this.title,
+    this.obscureText = false,
+    this.controller,
+    // this.isShowTitle = true,
+    // this.onFieldSubmitted,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: blackTextStyle.copyWith(
+            fontWeight: medium,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          obscureText: obscureText,
+          controller: controller,
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(14)),
+              contentPadding: const EdgeInsets.all(12)),
+        )
+      ],
+    );
+  }
+}
